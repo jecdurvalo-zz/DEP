69f8ebe220a05a2beee805e7ea2f4346868508de
Joana Durvalo <j.durvalo@gmail.com>
2018-03-16 15:43:13 -0300

    Aula 1

---
+++ Desafio/desafio1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP</title>
+    </head>    
+    
+    <body>
+        <h1>PHP</h1>
+        <?php
+            //Variáveis
+        $nome = "Gustavo";
+        $sobrenome = "Torrente";
+        $curso = "Sistemas para Internet";
+        $ano = 2018;
+        $faculdade = "FIAP";
+        
+        //Concatenação usando apenas 1 echo
+        
+        echo $nome . ' ' . $sobrenome . '<br>' .
+             $curso . ' ' . $ano . '<br>' .
+             $faculdade;
+        
+        ?>
+        
+        
+    </body>    
+</html>    
