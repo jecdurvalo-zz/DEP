5b72496376598ef95284c2372534c182d7a6f3d6
Joana Durvalo <j.durvalo@gmail.com>
2018-10-19 08:48:03 -0300

    update cadastro

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -5,5 +5,14 @@ module.exports.cadastro = function(application,req, res){
 module.exports.cadastrar = function(application, req, res){
     var dadosForm = req.body;
 
-    console.log(dadosForm);
+    req.assert('nome', 'Nome é obrigatório!').notEmpty();
+    req.assert('senha', 'Senha é obrigatória!').notEmpty();
+    req.assert('usuario', 'Usuário é obrigatório!').notEmpty();
+    req.assert('casa', 'Escolha um personagem!').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.send('Deu ruim!');
+    }
 }
