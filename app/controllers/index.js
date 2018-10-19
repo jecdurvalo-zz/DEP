54c91479d275428c957fa73f9cfa7a2bb817b4ca
Joana Durvalo <j.durvalo@gmail.com>
2018-10-19 11:10:19 -0300

    update changes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,28 @@
 module.exports.index = function(application,req, res){
-    res.render('index');
+    res.render('index', {
+    validacao: {}
+    });
+}
+
+module.exports.autenticar = function(application,req, res){
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuario não pode estar vazio').notEmpty();
+    req.assert('senha', 'Senha não pode estar vazio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render('index', {
+            validacao: erros,
+        });
+
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var UsuarioDAO = new application.app.models.UsuarioDAO(connection);
+
+
+    UsuarioDAO.autenticar(dadosForm, req, res);
 }
--- app/models/UsuarioDAO.js
+++ app/models/UsuarioDAO.js
@@ -1,18 +1,29 @@
-function UsuarioDAO(connection){
+function UsuarioDAO(connection) {
     //console.log("Carregou a função");
 
     this._connection = connection();
 }
 
-UsuarioDAO.prototype.inserirUsuario = function(usuario){
-    this._connection.open(function(err, mongoclient){
-        mongoclient.collection("usuarios", function(err, collection){
+UsuarioDAO.prototype.inserirUsuario = function (usuario) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("usuarios", function (err, collection) {
             collection.insert(usuario);
-            mongoclient.close();     
+            mongoclient.close();
         });
     });
 }
 
-module.exports = function(){
+UsuarioDAO.prototype.autenticar = function (usuario, req, res) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("usuarios", function (err, collection) {
+            collection.find(usuario).toArray(function(err, result){
+                console.log(result);
+            });
+            mongoclient.close();
+        });
+    });
+}
+
+module.exports = function () {
     return UsuarioDAO;
 }
